64f1c6bbc2581f58a5a5b4b9c463c9f5a6881583
MatejPC <matej113a@gmail.com>
2024-10-23 15:46:38 +0200

    .

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -15,17 +15,22 @@ function addDays(date, days) {
     newDate.setDate(date.getDate() + days);
     return newDate;
 }
-const filters = ["#0000FF", "#FF0000", "#008000" , "#FFFF00", "#FFA500", "#800080", "#FFC0CB", "#A52A2A", "#FF00FF", "#808080"];
+const filters = ["#0000FF", "#FF0000", "#008000", "#FFFF00", "#FFA500", "#800080", "#FFC0CB", "#A52A2A", "#FF00FF", "#808080"];
+
+const tmpdata = [{ "task_name": "task1", "description": "neke neke", "color": "#FF0000", "start_time": "2022-01-01T10:00:00", "end_time": "2022-01-01T12:00:00" }, { "task_name": "task2", "description": "neke neke", "color": "#FF00FF", "start_time": "2022-01-01T14:00:00", "end_time": "2022-01-01T16:00:00" }, { "task_name": "task3", "description": "neke neke", "color": "#00FF00", "start_time": "2022-01-01T18:00:00", "end_time": "2022-01-01T20:00:00" }, { "task_name": "task4", "description": "neke neke", "color": "#808080", "start_time": "2022-01-01T22:00:00", "end_time": "2022-01-01T23:00:00" }];
 
 function Calendar() {
     const location = useLocation();
     const [signedIn, setSignedIn] = useState(false);
     const [currentWeek, setCurrentWeek] = useState(getStartOfWeek(new Date()));
     const [selectedFilter, setSelectedFilter] = useState(null);
+    const [tasks, setTasks] = useState({});
 
     useEffect(() => {
         if (Cookie.get("signed_in_user") !== undefined) {
             setSignedIn(Cookie.get("signed_in_user"));
+            //api call to get le data and set it to tasks
+
         } else {
             setSignedIn(false);
         }
@@ -51,6 +56,17 @@ function Calendar() {
         timeSlots.push(`${hours.toString().padStart(2, '0')}:${minutes}`);
     }
 
+    function parseTime(times) {
+        var finalised_time = [];
+        for(var i in tmpdata){
+            var tmp=tmpdata[i];
+            let start=Date.parse(tmpdata[i].start_time);
+            let end=Date.parse(tmpdata[i].end);
+            
+            
+        }
+    }
+
     return (
         <div className="calendar-container">
             <div className="calendar-header">
@@ -59,17 +75,18 @@ function Calendar() {
                 <button className="change-week" onClick={handleNextWeek}>→</button>
             </div>
             <div className="filters">
-                <div onClick={() => setSelectedFilter(null)} className="clear-filter">Clear</div>
+                <div>Filter:</div>
                 {filters.map((filter, index) => (
                     <div
                         key={index}
                         onClick={() => setSelectedFilter(index)}
-                        className={`${selectedFilter==index ? 'active-filter' : 'filter'}`}
+                        className={`${selectedFilter === index ? 'active-filter' : 'filter'}`}
                         style={{ backgroundColor: filter }}
                     >
                         ⠀
                     </div>
                 ))}
+                <div className="clear-filter" onClick={() => setSelectedFilter(null)}>Clear</div>
             </div>
             <div className="calendar-grid-wrapper">
                 <div className="time-label-column">
@@ -87,7 +104,14 @@ function Calendar() {
                                         <>
                                             <div key={slotIndex} className="time-slot">
                                                 <div className="time-label-inline">{slot}</div> {/* Time label inside slot for small screens */}
-                                                <div className="content-area"></div> {/* Content will be added here later */}
+                                                <div className="content-area">
+                                                    {
+                                                        filters.map((filter, filterIndex) => (
+                                                            <>
+                                                                <b>⠀</b>
+                                                            </>
+                                                        ))}
+                                                </div> {/* Content will be added here later */}
                                             </div>
                                         </>
                                     ))}
